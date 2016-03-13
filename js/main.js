63afcb259a4b8d4b12e5c72ab7b1d699c55ec982
Alex Shapiro <aleksey.shapiro@gmail.com>
2016-03-13 15:15:11 -0400

    main data populated

--- js/main.js
+++ js/main.js
@@ -27,15 +27,54 @@
 		}
 		
 		function processData(data){
-			populateCityData(data.city);
+			populateMainData(data);
+			populateFourDays(data);
 		}
 
-		function populateCityData(data){
-			var o = $('.jumbotron .forecast-header');
+		function populateMainData(data){
+			var o = $('.jumbotron .container');
 			
-			//$('.day', o).text(data.);
-			//$('.date', o).text();
-			$('.location', o).text(data.name);
+			var date = new Date(data.list[0].dt * 1000),
+				day = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+			
+			$('.day', o).text(day[date.getUTCDay()]);
+			$('.date', o).text('Last update: ' + data.list[0].dt_txt);
+			$('.location .city', o).text(data.city.name);
+			$('.location .lat', o).text('(lat: ' + data.city.coord.lat);
+			$('.location .lon', o).text(', lon: ' + data.city.coord.lon + ')');
+			
+			var temp = (data.list[0].main.temp - 273.15) * 1.8 + 32,
+				units = 'f<sup>o</sup>';
+				
+			if(config.units === 'celsius'){
+				temp = (data.list[0].main.temp - 273.15);
+				units = 'C<sup>o</sup>';
+			}
+			
+			$('.degree .num', o).html(temp.toFixed(1) + ' ' + units);
+			
+			$('.degree .forecast-icon img', o).attr('src', 'http://openweathermap.org/img/w/' + data.list[0].weather[0].icon + '.png');
+			
+			$('.forecast-content span.hum').text(data.list[0].main.humidity);
+			$('.forecast-content span.wind').text(data.list[0].wind.speed);
+			$('.forecast-content span.dir').text(degToCompass(data.list[0].wind.deg));
+			//console.log(); data.list[0].main
+		}
+		
+		function populateFourDays(data){
+			$('.container .row .forecast').each(function(index){
+				var date = new Date(data.list[index+1].dt * 1000),
+					day = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+				
+				$(this).find('.forecast-header .day').text(day[date.getUTCDay()]);
+				console.log(index+1, day[date.getUTCDay()]);
+			});
+		}
+		
+		function degToCompass(num){
+			var n = Math.ceil((num / 22.5) + 0.5);
+			arr = ["N","NNE","NE","ENE","E","ESE", "SE", "SSE","S","SSW","SW","WSW","W","WNW","NW","NNW"];
+			return arr[n];
 		}
 		
 		getData();
